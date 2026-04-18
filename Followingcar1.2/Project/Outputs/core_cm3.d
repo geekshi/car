db169191b431523c6b9f5820da3dab037ce3f8ba
Shi Lei <leidotshi@qq.com>
2026-04-18 16:00:28 +0800

    remove following car code

--- Followingcar1.2/Project/Outputs/core_cm3.d
+++
@@ -1,2 +0,0 @@
-.\outputs\core_cm3.o: ..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.c
-.\outputs\core_cm3.o: D:\keil\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Followingcar1.2/Project/Outputs/startup_stm32f10x_hd.d
+++
@@ -1,1 +0,0 @@
-.\outputs\startup_stm32f10x_hd.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\startup\startup_stm32f10x_hd.s
